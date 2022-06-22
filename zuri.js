b167bf61259f715689ea8529284ff36fc5fe6bad
unknown <aminualaba9@gmail.com>
2022-06-22 12:37:34 +0100

    initial commit

---
+++ zuri.js
@@ -0,0 +1,26 @@
+
+// The operator input
+const calculator = prompt('Which Operation will you like to perform (  +, -, * or / ): ');
+
+// The operand input
+const number1 = parseFloat(prompt('Enter first number: '));
+const number2 = parseFloat(prompt('Enter second number: '));
+
+let result;
+
+// Using if...else if... else
+if (calculator == '+') {
+    result = number1 + number2;
+}
+else if (calculator == '-') {
+    result = number1 - number2;
+}
+else if (calculator == '*') {
+    result = number1 * number2;
+}
+else {
+    result = number1 / number2;
+}
+
+// The result
+console.log(`${number1} ${calculator} ${number2} = ${result}`);
